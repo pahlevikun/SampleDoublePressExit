fced8a92ec0355ceaa2d0dd29c73f16b31e4422a
Farhan Yuda Pahlevi <farhan@Pahlevis-MacBook-Pro.local>
2016-03-09 05:02:38 +0700

    Versi pertama

---
+++ app/src/main/java/com/ctproject/sampledoublepressexit/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ctproject.sampledoublepressexit;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Tekan lagi untuk keluar!", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
+}
